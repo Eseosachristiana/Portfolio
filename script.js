b93c3f8a4a08b705758a54994aec37461831a633
christiana eseosa <eseosachristiana@gmail.com>
2024-07-31 22:06:31 +0100

    added and removed some images

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.querySelector(".toggle_btn");
-const MenuBar = document.querySelector(".toggle_menu");
-
-menuButton.addEventListener("click", () => {
-    MenuBar.classList.toggle("show");
-});
